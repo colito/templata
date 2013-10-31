11ee183ff7296aca6e93e8e781d1ce91f1025639
Pride Mokhele <pride.mokhele@gmail.com>
2013-10-31 15:46:52 +0200

    * first commit

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+        </div>
+        <div id="footer">
+            <!-- <p>Copyright (c) 2013</p> -->
+        </div>
+    </body>
+</html>
+<?php ob_flush();?>
---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<?php ob_start();?>
+<html>
+<head>
+    <title><?php echo $page_name ?></title>
+
+    <link rel="stylesheet" href="css/normalize.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="css/grid.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+
+    <script type="text/javascript" src="js/jquery-2.0.3.min.js"></script>
+
+</head>
+
+<body>
+
+<div id="topper">
+
+    <div id="title">
+        <a href="index.php"><h2>Templata</h2></a>
+    </div>
+
+    <div id="links">
+        <a href="index.php">Home</a>&nbsp;
+        <a href="#">Link 1</a>&nbsp;
+        <a href="#">Link 2</a>&nbsp;
+    </div>
+
+</div>
+
+<div class="container_12 clearfix">
+
