95ec565743a9a148f493135f7b7e67b0e9e4dd73
Pride Mokhele <pride.mokhele@gmail.com>
2013-11-26 14:47:52 +0200

    * added function that pulls out specified content from the content folder

--- fns/file_handler.php
+++ fns/file_handler.php
@@ -1,7 +1,7 @@
 <?php
 class FileHandler
 {
-    public function get_external_file_content()
+    public function get_template_content()
     {
         $template_path = 'template/index.php';
 
@@ -15,5 +15,23 @@ class FileHandler
 
         return $data;
     }
+
+    public function get_content($file_name)
+    {
+        $content_path = '../content/'.$file_name;
+
+        $file_contents = fopen($content_path, "r");
+
+        while(!feof($file_contents))
+        {
+            $data = fread($file_contents, 500000);
+        }
+        fclose($file_contents);
+
+        return $data;
+    }
 }
+
+$file_handler = new FileHandler();
+
 ?>
