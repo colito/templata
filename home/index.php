ce5049e2c58807c2842492d8a1cd483bb8312327
Pride Mokhele <pride.mokhele@gmail.com>
2013-11-26 12:57:38 +0200

    * modified file to set it's header and footer using page handler functions

--- home/index.php
+++ home/index.php
@@ -1,8 +1,16 @@
 <?php
-$page_name = 'Home';
-require_once('../includes/header.php');
+require_once('../fns/page_handler.php');
+
+# Sets the header of the page.
+# Class has already been instantiated in ../fns/page_handler.php as $page_handler
+# Takes page name as parameter
+$page_handler->set_page_header('Home');
+
 ?>
 
 <!-- Content within div container class goes in here -->
 
-<?php require_once('../includes/footer.php')?>
+<?php
+# sets the footer of the page
+$page_handler->set_footer();
+?>
--- template/index.php
+++ template/index.php
@@ -1,8 +1,17 @@
 <?php
+require_once('../fns/page_handler.php');
+
+# Sets the header of the page.
+# Class has already been instantiated in ../fns/page_handler.php as $page_handler
+# Takes page name as parameter
 $page_name = '{page_name}';
-require_once('../includes/header.php');
+$page_handler->set_page_header($page_name);
+
 ?>
 
 {content}
 
-<?php require_once('../includes/footer.php')?>
+<?php
+# sets the footer of the page
+$page_handler->set_footer();
+?>
