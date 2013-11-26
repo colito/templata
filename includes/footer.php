f10c0a2b1937aad37b556fc1661dc84b9552f9ac
Pride Mokhele <pride.mokhele@gmail.com>
2013-11-26 11:37:21 +0200

    * added mobile menu toggle jquery setup

--- includes/footer.php
+++ includes/footer.php
@@ -3,5 +3,13 @@
             <!-- <p>Copyright (c) 2013</p> -->
         </div>
     </body>
+    <!-- Menu toggle for smart phones -->
+    <script type="text/javascript" charset="utf-8">
+        $(document).ready(function(){
+            $("#switch").click(function(){
+                $("#panel").slideToggle(250);
+                });
+        });
+    </script>
 </html>
 <?php ob_flush();?>
