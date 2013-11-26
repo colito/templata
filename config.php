78cdf1381d8245ba1f81d7a717dd6e69b8d712ee
Pride Mokhele <pride.mokhele@gmail.com>
2013-11-26 11:34:51 +0200

    * modified definitions

--- config.php
+++ config.php
@@ -18,8 +18,13 @@ define('main_include', '../includes/');
 define('main_header', '../includes/header.php');
 define('main_footer', '../includes/footer.php');
 
+define('main_fns', '../fns/');
+
 define('main_images', '../images/');
 define('main_css', '../css/');
+define('main_css_reset', '../css/normalize.css');
+define('main_css_grid', '../css/grid.css');
+define('main_css_style', '../css/style.css');
 define('main_js', '../js/');
 define('main_jquery', '../js/jquery-2.0.3.min.js');
 
