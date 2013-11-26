d7530da06e8b443b963e346ecc24d485947bb6cc
Pride Mokhele <pride.mokhele@gmail.com>
2013-11-26 10:10:02 +0200

    *

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-    $page_name = 'Page Name';
-    require_once('includes/header.php');
+header('Location: home');
 ?>
-
-<!-- Content within div container class goes in here -->
-
-<?php require_once('includes/footer.php')?>
