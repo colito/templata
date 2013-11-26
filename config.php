a8a4e311a77acd8dfab6392299539f0863cc930c
Pride Mokhele <pride.mokhele@gmail.com>
2013-11-26 10:05:34 +0200

    * added config file to with site name, root function and definitions

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+Class Config
+{
+    public $site_name = "Templata";
+
+    public function file_root()
+    {
+        $root = $_SERVER['DOCUMENT_ROOT'];
+        return $root;
+    }
+}
+
+# navigation links
+define('navi_home', '../home/');
+
+# included file paths
+define('main_include', '../includes/');
+define('main_header', '../includes/header.php');
+define('main_footer', '../includes/footer.php');
+
+define('main_images', '../images/');
+define('main_css', '../css/');
+define('main_js', '../js/');
+define('main_jquery', '../js/jquery-2.0.3.min.js');
+
+?>
