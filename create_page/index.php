e55ec30dc0c5ea07707847387781068415ddf8a2
Pride Mokhele <pride.mokhele@gmail.com>
2013-11-26 15:43:37 +0200

    * testing the new file create function

---
+++ create_page/index.php
@@ -0,0 +1,25 @@
+<?php
+$page_name = 'Create new page';
+require_once('../includes/header.php');
+require_once('../fns/file_handler.php');
+$file_handler = new FileHandler();
+
+$new_file_content = $file_handler->get_template_content();
+$new_file_content = str_replace('{page_name}', 'Page Name', $new_file_content);
+$new_file_content = str_replace('{content}', 'Hello World', $new_file_content);
+
+//var_dump($new_file_content);
+
+$fp = fopen('data.txt', 'w');
+$string = 'This is my test string';
+$result = $file_handler->fwrite_stream($fp, $string);
+
+?>
+
+<div id="content">
+    <h2>Create Page</h2>
+    <br>
+    <p>Page name: <input type="text" name="new_page"></p>
+</div>
+
+<?php require_once('../includes/footer.php') ?>
