f85ab4e0dd0d252324fc74e3de117f2e0dbfc349
Pride Mokhele <pride.mokhele@gmail.com>
2013-12-14 14:56:05 +0200

    * amended css links to be defined from the config file

--- includes/header.php
+++ includes/header.php
@@ -13,9 +13,10 @@ $config = new Config();
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
 
-    <link rel="stylesheet" href="<?php echo main_css?>normalize.css" type="text/css" media="screen">
-    <link rel="stylesheet" href="<?php echo main_css?>grid.css" type="text/css" media="screen">
-    <link rel="stylesheet" href="<?php echo main_css?>style.css" type='text/css' media="screen">
+    <link rel="stylesheet" href="<?php echo main_css_reset?>" type="text/css" media="screen">
+    <link rel="stylesheet" href="<?php echo main_css_normalize?>" type="text/css" media="screen">
+    <link rel="stylesheet" href="<?php echo main_css_grid?>" type="text/css" media="screen">
+    <link rel="stylesheet" href="<?php echo main_css_style?>" type='text/css' media="screen">
 
     <!-- JQuery 2.0.3-->
     <script type="text/javascript" src="<?php echo main_jquery?>"></script>
