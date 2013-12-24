5b3a50c6b6df21127168ba769f438335521c3c91
Pride Mokhele <pride.mokhele@gmail.com>
2013-12-24 16:10:40 +0200

    * reset home

--- home/index.php
+++ home/index.php
@@ -6,24 +6,9 @@ require_once('../fns/file_handler.php');
 # Class has already been instantiated in ../fns/page_handler.php as $page_handler
 # Takes page name as parameter
 $page_handler->set_page_header('Home');
-
-
-//echo basename('etc/one/page.php');
-//echo dirname('etc/one/page.php');
 ?>
 
-<!-- Content within div container class goes in here -->
-<?php
-    if(!empty($_GET['content']))
-    {
-        $file_name = $_GET['content'];
-        //$x = $file_handler->get_content($file_name);
-        echo $file_handler->get_content($file_name);
-
-        //var_dump($x);
-    }
-
-?>
+<!-- Content goes here. Body and container have already been included, continue from there. -->
 
 <?php
 # sets the footer of the page
