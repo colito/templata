aca980b0aae0618b33c2306e0a3eef290c0515a6
Pride Mokhele <pride.mokhele@gmail.com>
2014-01-07 12:22:25 +0200

    used to test link_handler() output

--- home/index.php
+++ home/index.php
@@ -10,8 +10,14 @@ $page_handler->set_page_header('Home');
 ?>
 
 <!-- Content goes here. Body and container have already been included, continue from there. -->
+<?php
+
+    $x = $page_handler->link_handler();
+    var_dump($x);
+
+?>
 
 <?php
 # sets the footer of the page
-$page_handler->set_footer();
+$page_handler->set_page_footer();
 ?>
