3003314d11f88f98f7e7045418c754dc4439dd8f
Pride Mokhele <pride.mokhele@gmail.com>
2014-01-08 11:47:15 +0200

    * modified navigation links

--- config.php
+++ config.php
@@ -11,9 +11,9 @@ Class Config
 # All user links should be defined as follows:
 # *Link Name:actual_link.file_extention
 
-*Home:home
-*Link 1:#
-*Create:create_page
+*Home:../home
+*Link 1:../#
+*Create:../create_page
 
 -->
 
