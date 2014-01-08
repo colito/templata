8ea23bd012f2ca464297fccff3546a6618812c42
Pride Mokhele <pride.mokhele@gmail.com>
2014-01-08 13:25:39 +0200

    * fixed page name

--- content/index.php
+++ content/index.php
@@ -5,7 +5,7 @@ require_once(APP_ROOT_DIR.'/fns/file_handler.php');
 # Sets the header of the page.
 # Class has already been instantiated in ../fns/page_handler.php as $page_handler
 # Takes page name as parameter
-$page_handler->set_page_header('Home');
+$page_handler->set_page_header('Content');
 
 ?>
 
