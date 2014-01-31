535614aeb91b76639032ef827a408bd5ad100683
Pride Mokhele <pride.mokhele@gmail.com>
2014-01-31 17:01:59 +0200

    * added new test navigation link and changed most of the path definitions to make them more accsessible

--- config.php
+++ config.php
@@ -14,6 +14,7 @@ Class Config
 *Home:home
 *Create:create_page
 *Content:content
+*Test:home/test
 
 -->
 
@@ -33,22 +34,22 @@ define('navi_admin', main_admin);
 
 # included file paths ***************************************/
 
-define('main_content', '../content');
+define('main_content', APP_ROOT_DIR.'/content');
 
-define('main_include', '../includes/');
-define('main_header', '../includes/header.php');
-define('main_footer', '../includes/footer.php');
+define('main_include', APP_ROOT_DIR.'/includes/');
+define('main_header', APP_ROOT_DIR.'/includes/header.php');
+define('main_footer', APP_ROOT_DIR.'/includes/footer.php');
 
-define('main_fns', '../fns/');
+define('main_fns', APP_ROOT_DIR.'/fns/');
 
-define('main_images', '../images/');
-define('main_css', '../css/');
-define('main_css_reset', '../css/reset.css');
-define('main_css_normalize', '../css/normalize.css');
-define('main_css_grid', '../css/grid.css');
-define('main_css_style', '../css/style.css');
-define('main_js', '../js/');
-define('main_jquery', '../js/jquery-2.0.3.min.js');
+define('main_images', '/images/');
+define('main_css', '/css/');
+define('main_css_reset', '/css/reset.css');
+define('main_css_normalize', '/css/normalize.css');
+define('main_css_grid', '/css/grid.css');
+define('main_css_style', '/css/style.css');
+define('main_js', '/js/');
+define('main_jquery', '/js/jquery-2.0.3.min.js');
 
 ?>
 
