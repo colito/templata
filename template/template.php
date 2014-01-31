242252cee34fb35333ea8c14c73d40f75456f371
Pride Mokhele <pride.mokhele@gmail.com>
2014-01-31 17:07:38 +0200

    * redifined template to the new functioning structure

--- template/template.php
+++ template/template.php
@@ -1,17 +1,23 @@
 <?php
-require_once('../fns/page_handler.php');
+require_once('../config.php');
+require_once(APP_ROOT_DIR.'/fns/page_handler.php');
+require_once(APP_ROOT_DIR.'/fns/file_handler.php');
 
 # Sets the header of the page.
 # Class has already been instantiated in ../fns/page_handler.php as $page_handler
-# Takes page name as parameter
-$page_name = '{page_name}';
-$page_handler->set_page_header($page_name);
+# Takes page name as parameter and the relative root for main navigation
+$depth = $file_handler->relative_root(dirname(__FILE__));
+$page_handler = new PageHandler();
+$page_handler->set_page_header('Template', $depth);
 
 ?>
 
-{content}
+<!-- Content goes here. Body and container have already been included, continue from there. -->
+<?php
+var_dump('Hello');
+?>
 
 <?php
 # sets the footer of the page
-$page_handler->set_footer();
+//$page_handler->set_page_footer();
 ?>
