88467ac6084fe21f32926927284c3ecc2e7d7f5c
Pride Mokhele <pride.mokhele@gmail.com>
2014-01-31 15:54:48 +0200

    * now complies with relative root

--- home/index.php
+++ home/index.php
@@ -4,14 +4,15 @@ require_once(APP_ROOT_DIR.'/fns/file_handler.php');
 
 # Sets the header of the page.
 # Class has already been instantiated in ../fns/page_handler.php as $page_handler
-# Takes page name as parameter
-$page_handler->set_page_header('Home');
+# Takes page name as parameter and the relative root for main navigation
+$depth = $file_handler->relative_root(dirname(__FILE__));
+$page_handler->set_page_header('Home', $depth);
 
 ?>
 
 <!-- Content goes here. Body and container have already been included, continue from there. -->
 <?php
-
+   
 ?>
 
 <?php
