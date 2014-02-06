b62299b46554c80e5ec08c03bcfb885e72649e9b
Pride Mokhele <pride.mokhele@gmail.com>
2014-02-06 12:11:13 +0200

    * tested link that uses relative link routing

--- home/test/index.php
+++ home/test/index.php
@@ -1,29 +1,25 @@
 <?php
+
 require_once('../../config.php');
 require_once(APP_ROOT_DIR.'/fns/page_handler.php');
-require_once(APP_ROOT_DIR.'/fns/file_handler.php');
 
 # Sets the header of the page.
 # Class has already been instantiated in ../fns/page_handler.php as $page_handler
 # Takes page name as parameter and the relative root for main navigation
-$depth = $file_handler->relative_root(dirname(__FILE__));
 $page_handler = new PageHandler();
+$depth = $page_handler->relative_link_path(dirname(__FILE__));
 $page_handler->set_page_header('Test', $depth);
 
 ?>
 
 <!-- Content goes here. Body and container have already been included, continue from there. -->
 <?php
-//echo dirname(__FILE__);
 
-//$create_page = $file_handler->relative_root(dirname(__FILE__)) . 'content';
-//echo '<a href="'.$create_page.'">Create Page</a>';
-echo 'Hello!!';
+    echo '<a href="'.$depth.'content/construction">View Construction Page</a>';
 
-//var_dump($page_handler->navigation_menu($depth));
 ?>
 
 <?php
 # sets the footer of the page
-//$page_handler->set_page_footer();
+$page_handler->set_page_footer();
 ?>
