ce08663a702709dedde934b6139b4c303a40f6da
Pride Mokhele <pride.mokhele@gmail.com>
2014-02-06 12:07:56 +0200

    * experimented with create_slug() function

--- home/index.php
+++ home/index.php
@@ -1,20 +1,25 @@
 <?php
 require_once('../config.php');
 require_once(APP_ROOT_DIR.'/fns/page_handler.php');
-require_once(APP_ROOT_DIR.'/fns/file_handler.php');
 
 # Sets the header of the page.
 # Class has already been instantiated in ../fns/page_handler.php as $page_handler
 # Takes page name as parameter and the relative root for main navigation
-$depth = $file_handler->relative_root(dirname(__FILE__));
 $page_handler = new PageHandler();
+$depth = $page_handler->relative_link_path(dirname(__FILE__));
 $page_handler->set_page_header('Home', $depth);
 
 ?>
 
 <!-- Content goes here. Body and container have already been included, continue from there. -->
 <?php
-    var_dump('Hello');
+    $string = 'how to say hello';
+    function create_slug($string){
+        $slug=preg_replace('/[^A-Za-z0-9-]+/', '-', strtolower($string));
+        return $slug;
+    }
+    echo create_slug('DOES THIS THING WORK OR NOT 100 2333 **56 ] {');
+    //returns 'does-this-thing-work-or-not'
 ?>
 
 <?php
