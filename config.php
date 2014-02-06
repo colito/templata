930694c89ce45dd32035fa5176ab163e67e98460
Pride Mokhele <pride.mokhele@gmail.com>
2014-02-06 12:01:11 +0200

    * moved definitions from config.php into includes/definitions.php

--- config.php
+++ config.php
@@ -2,6 +2,8 @@
 Class Config
 {
     public $site_name = "Templata";
+    public $user_name = '';
+    public $password = '';
 }
 ?>
 
@@ -10,6 +12,7 @@ Class Config
 
 # All user links should be defined as follows:
 # *Link Name:actual_link.file_extension
+# This might be moved to a more appropriate location
 
 *Home:home
 *Create:create_page
@@ -19,37 +22,15 @@ Class Config
 -->
 
 <?php
-# DEFINITIONS ***********************************************************************/
 
+# define application root
 define('APP_ROOT_DIR', dirname(__FILE__));
 
-define('main_admin', '../admin_dunamis');
+# coming soon...
+//define('main_admin', '../admin_dunamis');
 
-
-# navigation links *******************************************/
-# now replaced by navigation links above
-define('navi_home', 'home');
-define('navi_admin', main_admin);
-
-
-# included file paths ***************************************/
-
-define('main_content', APP_ROOT_DIR.'/content');
-
-define('main_include', APP_ROOT_DIR.'/includes/');
-define('main_header', APP_ROOT_DIR.'/includes/header.php');
-define('main_footer', APP_ROOT_DIR.'/includes/footer.php');
-
-define('main_fns', APP_ROOT_DIR.'/fns/');
-
-define('main_images', '/images/');
-define('main_css', '/css/');
-define('main_css_reset', '/css/reset.css');
-define('main_css_normalize', '/css/normalize.css');
-define('main_css_grid', '/css/grid.css');
-define('main_css_style', '/css/style.css');
-define('main_js', '/js/');
-define('main_jquery', '/js/jquery-2.0.3.min.js');
+# include definitions
+require_once(APP_ROOT_DIR.'/includes/definitions.php');
 
 ?>
 
---
+++ includes/definitions.php
@@ -0,0 +1,32 @@
+<?php
+# DEFINITIONS ***********************************************************************/
+
+
+
+# navigation links *******************************************/
+# now replaced by navigation links above
+define('navi_home', 'home');
+define('navi_admin', main_admin);
+
+
+# included file paths ***************************************/
+
+define('main_content', APP_ROOT_DIR.'/content');
+
+define('main_include', APP_ROOT_DIR.'/includes/');
+define('main_header', APP_ROOT_DIR.'/includes/header.php');
+define('main_footer', APP_ROOT_DIR.'/includes/footer.php');
+
+define('main_fns', APP_ROOT_DIR.'/fns/');
+
+define('main_images', '/images/');
+define('main_css', '/css/');
+define('main_css_reset', '/css/reset.css');
+define('main_css_normalize', '/css/normalize.css');
+define('main_css_grid', '/css/grid.css');
+define('main_css_style', '/css/style.css');
+define('main_js', '/js/');
+define('main_jquery', '/js/jquery-2.0.3.min.js');
+
+
+?>
