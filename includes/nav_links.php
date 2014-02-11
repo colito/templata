8cd8c8662340fd71743e0662468f8ac920921b54
Pride Mokhele <pride.mokhele@gmail.com>
2014-02-11 13:36:11 +0200

    * made link compliant with .htaccess file

---
+++ includes/nav_links.php
@@ -0,0 +1,13 @@
+<!--
+            NAVIGATION LINKS
+
+# All user links should be defined as follows:
+# *Link Name:actual_link.file_extension
+# This might be moved to a more appropriate location
+
+*Home:home
+*Create:create_page
+*Content:article/
+*Test:home/test
+
+-->
