aeae398e0446c33970920311635d97f7924f0bb4
Pride Mokhele <pride.mokhele@gmail.com>
2014-02-17 14:22:23 +0200

    * moved css, js and fns directories to 'libs' directory

---
+++ libs/fns/file_handler.php
@@ -0,0 +1,116 @@
+<?php
+class FileHandler
+{
+    # takes a snapshot of /template/template.php and creates a new file from it.
+    public function get_template_content()
+    {
+        $config = new Config();
+        $active_template = $config->active_template;
+
+        $template_path = APP_ROOT_DIR.'/template/'.$active_template.'/index.php';
+
+        $file_contents = fopen($template_path, "r");
+
+        while(!feof($file_contents))
+        {
+            $data = fread($file_contents, 500000);
+        }
+        fclose($file_contents);
+
+        return $data;
+    }
+
+    # takes a hold of the contents within the contend directory.
+    # better to use require_once() instead.
+    public function get_content($file_name)
+    {
+        $content_path = '../content/'.$file_name;
+
+        $file_contents = fopen($content_path, "r");
+
+        while(!feof($file_contents))
+        {
+            $data = fread($file_contents, 500000);
+        }
+        fclose($file_contents);
+
+        return $data;
+    }
+
+    public function require_content($depth, $file_name)
+    {
+        $file_path = $depth.'content/'.$file_name;
+        $content = file_get_contents($file_path);
+        return $content;
+    }
+
+    # creates a new file as specified by the user and places it within the content directory
+    # only works on linux
+    public function create_new_page($new_file)
+    {
+        # file path and file name
+        $new_file_name = basename($new_file);
+        $dir = '../content/';
+        $dir .= dirname($new_file);
+
+        # create the specified file
+        shell_exec('mkdir -p '.$dir);
+        shell_exec('touch '.$dir.'/'.$new_file_name);
+
+        $path_with_query= $dir;
+        $path=explode('/',$path_with_query);
+        //$filename=basename($path[0]);
+        $query=$path;
+        $change_file_perm = $path[0].'/'.$path[1].'/'.$path[2];
+
+//        var_dump($query);
+//        var_dump($change_file_perm);
+
+        # change the files permissions
+        //$x = chmod($dir.'/'.$new_file_name, 0777);
+        $x = chmod($change_file_perm, 0777);
+        //var_dump($x);
+    }
+
+    public function chmod_R($path, $filemode, $dirmode) {
+        if (is_dir($path) ) {
+            if (!chmod($path, $dirmode)) {
+                $dirmode_str=decoct($dirmode);
+                print "Failed applying filemode '$dirmode_str' on directory '$path'\n";
+                print "  `-> the directory '$path' will be skipped from recursive chmod\n";
+                return;
+            }
+            $dh = opendir($path);
+            while (($file = readdir($dh)) !== false) {
+                if($file != '.' && $file != '..') {  // skip self and parent pointing directories
+                    $fullpath = $path.'/'.$file;
+                    chmod_R($fullpath, $filemode,$dirmode);
+                }
+            }
+            closedir($dh);
+        } else {
+            if (is_link($path)) {
+                print "link '$path' is skipped\n";
+                return;
+            }
+            if (!chmod($path, $filemode)) {
+                $filemode_str=decoct($filemode);
+                print "Failed applying filemode '$filemode_str' on file '$path'\n";
+                return;
+            }
+        }
+    }
+
+    public function fwrite_stream($fp, $string) {
+
+        for ($written = 0; $written < strlen($string); $written += $fwrite) {
+            $fwrite = fwrite($fp, substr($string, $written));
+            if ($fwrite === false) {
+                return $written;
+            }
+        }
+        return $written;
+    }
+}
+
+?>
---
+++ libs/fns/page_handler.php
@@ -0,0 +1,192 @@
+<?php
+//require_once('../config.php');
+class PageHandler
+{
+
+    public function set_page_header($page_name, $depth)
+    {
+        # $page_name is already in '../includes/header.php'
+        //return require_once('../includes/header.php');
+        return require_once(main_header);
+        //return 'Another hello!';
+    }
+
+    public function set_page_footer()
+    {
+        return require_once(main_footer);
+    }
+
+    # needs to determine depth levels from number of directories not number of overall files
+    public function relative_link_path($file_root_path)
+    {
+        $app_root = APP_ROOT_DIR;
+        $relative_path = str_replace($app_root.'/', '', $file_root_path);
+        $path_array = explode('/', $relative_path);
+        //$depth = count($path_array);
+
+        $depth = '';
+
+        foreach($path_array as $level)
+        {
+            $level = '../';
+            $depth .= $level;
+        }
+
+        return $depth;
+    }
+
+    # ref: http://css-tricks.com/snippets/php/create-url-slug-from-post-title/
+    public function create_slug($string){
+        $slug=preg_replace('/[^A-Za-z0-9-]+/', '-', strtolower($string));
+        return $slug;
+    }
+
+    public function link_handler()
+    {
+        $config = new Config();
+        $links = $config->navigation_links;
+        $links_file = APP_ROOT_DIR.$links;
+        $lines = file($links_file, FILE_IGNORE_NEW_LINES);
+
+        # array clean-up
+        $clean_lines = array();
+        foreach($lines as $line)
+        {
+            # checks array for an occupied array index.
+            if(!empty($line))
+            {
+                # checks if the first character of the string is *
+                if(substr($line, 0, 1) == '*')
+                {
+                    $clean_lines[] = substr($line, 0);
+                }
+            }
+        }
+
+        # getting the link names
+        $link_names = array();
+        foreach($clean_lines as $link_name)
+        {
+            # fishes out the colon position to determine the reading endpoint.
+            $colon_position = strpos($link_name, ':') - 1;
+            $link_names[] = substr($link_name, 1, $colon_position);
+        }
+
+        # getting the actual links
+        $actual_links = array();
+        foreach($clean_lines as $actual_link)
+        {
+            # reads the entire line after the colon
+            $colon_position = strpos($actual_link, ':') + 1;
+            $actual_links[] = substr($actual_link, $colon_position);
+        }
+
+        # putting everything together in one array
+        $nav_links = array();
+        for($i=0; $i<count($link_names); $i++)
+        {
+            $nav_links['nav_links'][$i]['link_name'] = $link_names[$i] ;
+            $nav_links['nav_links'][$i]['link'] = $actual_links[$i];
+        }
+
+        return $nav_links;
+    }
+
+    public function navigation_menu($depth = null)
+    {
+        $x = $this->link_handler();
+
+        echo '<ul>';
+        foreach($x['nav_links'] as $y)
+        {
+            echo '<li><a href=" '.$depth.$y['link'].' ">'.$y['link_name'].'</a></li>';
+        }
+        echo '</ul>';
+    }
+
+    public function navigation_menu2($depth = null)
+    {
+        $x = $this->link_handler();
+
+        $output = '';
+        foreach($x['nav_links'] as $y)
+        {
+            $output .= '<li><a href="'.$depth.$y['link'].'">'.$y['link_name'].'</a></li>';
+        }
+
+        $overall_output = '<ul>';
+        $overall_output .= $output;
+        $overall_output .='</ul>';
+
+        return $overall_output;
+    }
+
+    public function right_click_status($status)
+    {
+        if($status == 0)
+        {
+            $right_click_status = 'oncontextmenu="return false"';
+        }
+        elseif($status == 1)
+        {
+            $right_click_status = '';
+        }
+
+        return $right_click_status;
+    }
+
+    public function get_content($depth, $file_name)
+    {
+        $file_path = $depth.'content/'.$file_name;
+        $content = file_get_contents($file_path);
+        return $content;
+    }
+
+    public function main_libraries()
+    {
+
+    }
+
+    public function output_template($page_name, $depth, $body_content = null)
+    {
+        $page_title = $page_name;
+        $config = new Config();
+        $active_template = $config->active_template;
+        $app_name = $config->app_name;
+
+        $template_path = APP_ROOT_DIR.'/template/'.$active_template.'/index.php';
+        $template_res = $depth.'template/'.$active_template;
+        $main_images = $depth.$config->main_images_directory;
+        $favicon = $main_images.'/favicon/favicon.ico';
+
+        $file_contents = fopen($template_path, "r");
+
+        while(!feof($file_contents))
+        {
+            $data = fread($file_contents, 500000);
+        }
+        fclose($file_contents);
+
+        #
+        $include = str_replace('{app_name}', $app_name, $data);
+        $include = str_replace('{page_title}', $page_title, $include);
+
+        # Pathing
+        $include = str_replace('{relative}', $depth, $include);
+        $include = str_replace('{favicon}', $favicon, $include);
+        $include = str_replace('{templata_libs}', $template_res, $include);
+        $include = str_replace('{templata_images}', $main_images, $include);
+
+        # Body
+        $include = str_replace('{right_click}', $this->right_click_status($config->right_click), $include);
+        $include = str_replace('{body_content}', $body_content, $include);
+
+        # Navigation
+        $include = str_replace('{navigation_menu}', $this->navigation_menu2($depth), $include);
+        $include = str_replace('{mobi_navigation_menu}', $this->navigation_menu2($depth), $include);
+
+        echo $include;
+    }
+}
+
+?>
