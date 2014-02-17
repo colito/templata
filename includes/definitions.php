f326c01fcb64aecc4a3e2db151c566ba7b516cd4
Pride Mokhele <pride.mokhele@gmail.com>
2014-02-17 14:29:53 +0200

    * modified fns definition

--- includes/definitions.php
+++ includes/definitions.php
@@ -17,16 +17,16 @@ define('main_include', APP_ROOT_DIR.'/includes/');
 define('main_header', APP_ROOT_DIR.'/includes/header.php');
 define('main_footer', APP_ROOT_DIR.'/includes/footer.php');
 
-define('main_fns', APP_ROOT_DIR.'/fns/');
+define('T_FNS', APP_ROOT_DIR.'/libs/fns/');
 
 define('main_images', '/images/');
-define('main_css', '/css/');
-define('main_css_reset', '/css/reset.css');
-define('main_css_normalize', '/css/normalize.css');
-define('main_css_grid', '/css/grid.css');
-define('main_css_style', '/css/style.css');
+define('main_css', '/libs/css/');
+define('main_css_reset', '/libs/css/reset.css');
+define('main_css_normalize', '/libs/css/normalize.css');
+define('main_css_grid', '/libs/css/grid.css');
+define('main_css_style', '/libs/css/style.css');
 define('main_js', '/js/');
-define('main_jquery', '/js/jquery-2.0.3.min.js');
+define('main_jquery', '/libs/js/jquery-2.0.3.min.js');
 
 
 ?>
