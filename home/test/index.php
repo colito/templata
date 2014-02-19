2c4ca8f71ca864f259c7690951bace9a098e1706
Pride Mokhele <pride.mokhele@gmail.com>
2014-02-19 16:33:27 +0200

    * now displays new test content by specifying folder and file name as parameters

--- home/test/index.php
+++ home/test/index.php
@@ -6,10 +6,11 @@ $page_handler = new PageHandler();
 
 $page_title = 'Test';
 $relative_path_depth = $page_handler->relative_link_path(dirname(__FILE__));
-$body_content = $page_handler->get_content($relative_path_depth, 'sample/index2.php');
+$body_content = $page_handler->get_content($relative_path_depth, 'theone','solutions.php');
 
-$page_handler->output_template($page_title, $relative_path_depth, $body_content);
+$page_handler->output_page($page_title, $relative_path_depth, $body_content);
 
-var_dump($page_handler->get_resource($relative_path_depth, 'jquery'));
+//var_dump($page_handler->get_resource($relative_path_depth, 'jquery'));
 //var_dump($page_handler->get_jquery($relative_path_depth));
+//var_dump($page_handler->main_libraries($relative_path_depth));
 ?>
