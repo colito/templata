980f0b7a832a9e992f85ad7519a06e21705ae379
Pride Mokhele <pride.mokhele@gmail.com>
2014-02-20 17:01:47 +0200

    * uses  key and value to view content pages

--- home/test/index.php
+++ home/test/index.php
@@ -2,15 +2,28 @@
 require_once('../../config.php');
 require_once(T_FNS.'page_handler.php');
 
+$config = new Config();
 $page_handler = new PageHandler();
 
 $page_title = 'Test';
 $relative_path_depth = $page_handler->relative_link_path(dirname(__FILE__));
-$body_content = $page_handler->get_content($relative_path_depth, 'theone','solutions.php');
+
+$query_string_array = explode('=', $_SERVER['QUERY_STRING']);
+
+if(isset($_GET))
+{
+    $category =  $query_string_array[0];
+    $landing_page = $query_string_array[1];
+}
+else
+{
+    echo 'Sorry';
+    $category = $config->landing_directory;
+    $landing_page = $config->landing_page;
+}
+
+$body_content = $page_handler->get_content($relative_path_depth, $category,$landing_page.'.php');
 
 $page_handler->output_page($page_title, $relative_path_depth, $body_content);
 
-//var_dump($page_handler->get_resource($relative_path_depth, 'jquery'));
-//var_dump($page_handler->get_jquery($relative_path_depth));
-//var_dump($page_handler->main_libraries($relative_path_depth));
 ?>
