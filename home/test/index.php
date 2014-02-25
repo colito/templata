9ad8651baa0aa0892e305167d7ff7bac7c3b4899
Pride Mokhele <pride.mokhele@gmail.com>
2014-02-25 16:54:38 +0200

    * added handling for GET parameters

--- home/test/index.php
+++ home/test/index.php
@@ -5,25 +5,21 @@ require_once(T_FNS.'page_handler.php');
 $config = new Config();
 $page_handler = new PageHandler();
 
-$page_title = 'Test';
 $relative_path_depth = $page_handler->relative_link_path(dirname(__FILE__));
-
-$query_string_array = explode('=', $_SERVER['QUERY_STRING']);
 
 if(isset($_GET))
 {
-    $category =  $query_string_array[0];
-    $landing_page = $query_string_array[1];
+    $category =  $_GET['category'];
+    $landing_page = $_GET['id'];
 }
 else
 {
-    echo 'Sorry';
     $category = $config->landing_directory;
     $landing_page = $config->landing_page;
 }
 
 $body_content = $page_handler->get_content($relative_path_depth, $category,$landing_page.'.php');
 
-$page_handler->output_page($page_title, $relative_path_depth, $body_content);
+$page_handler->output_page($relative_path_depth, $body_content);
 
 ?>
