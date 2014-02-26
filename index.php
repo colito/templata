ef1dfd3a41e9767c9343d1581bd7522a3f11f1c7
Pride Mokhele <pride.mokhele@gmail.com>
2014-02-26 11:58:38 +0200

    * moved in code from home/test/index.php

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
-header('Location: home');
+
+require_once('config.php');
+require_once(T_FNS.'page_handler.php');
+
+$config = new Config();
+$page_handler = new PageHandler();
+
+$relative_path_depth = $page_handler->relative_link_path(dirname(__FILE__));
+
+if(!empty($_GET))
+{
+    $category =  $_GET['category'];
+    $landing_page = $_GET['id'];
+}
+else
+{
+    $category = $config->landing_directory;
+    $landing_page = $config->landing_page;
+}
+
+$body_content = $page_handler->get_content($relative_path_depth, $category,$landing_page.'.php');
+
+$page_handler->output_page($relative_path_depth, $body_content);
+
 ?>
