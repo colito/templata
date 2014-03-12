475a9a120e754e553b460cc7d5b489cf3872845c
Pride Mokhele <pride.mokhele@gmail.com>
2014-03-12 11:57:57 +0200

    * used to test link new link extraction functions in the PageHandler class

--- content/sample/index.php
+++ content/sample/index.php
@@ -1,1 +1,20 @@
+<!--
+CSS:
+[css:reset.css]
+[css:normalize.css]
+[css:grid.css]
+[css:style.css]
+
+[page:page name]
+
+<a href="jump/what.php">What's Good?!</a>
+<a href="jump/one.php">What's Good?!</a>
+<a href="jump/two.php">What's Good?!</a>
+
+<script type="text/javascript" src="js/javascript.js">
+<script type="text/javascript" src="js/jquery.js">
+<script type="text/javascript" src="js/javascript.js">
+<script type="text/javascript" src="js/javascript.js">
+-->
+
 <h2>Hello World</h2>
