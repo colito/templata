eaafe964562c67df81aecdb631868aec70025cac
Pride Mokhele <pride.mokhele@gmail.com>
2014-04-03 15:58:27 +0200

    * template will now use it's own navigation links file

---
+++ templates/theone/includes/nav_links.php
@@ -0,0 +1,40 @@
+<!--
+            NAVIGATION LINKS
+
+# All user links should be defined as follows:
+# *Link Name:actual_link.file_extension
+# This might be moved to a more appropriate location
+
+# Templata Basic
+Home:
+Content:
+Test:
+
+# The One
+Home:?category=theone&id=index
+Solutions:?category=theone&id=solutions
+Contact:?category=theone&id=contact
+Hello:?category=theone&id=hello
+
+# Greenwhich
+*Home:greenwhich/home.html
+*Products:greenwhich/products.html
+*Calender:#
+*Contact:greenwhich/contact.html
+
+
+
+
+            CASCADING STYLE SHEET FILES
+
+[css:reset.css]
+[css:normalize.css]
+[css:grid.css]
+[css:style.css]
+
+
+            JAVASCRIPT FILES
+
+[js:jquery]
+
+-->
