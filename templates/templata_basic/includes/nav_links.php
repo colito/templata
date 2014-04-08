0660a24bd233062f5f3c8300cbfd571de19f5aca
Pride Mokhele <pride.mokhele@gmail.com>
2014-04-08 16:35:17 +0200

    * modified navigation and css links

--- templates/templata_basic/includes/nav_links.php
+++ templates/templata_basic/includes/nav_links.php
@@ -5,24 +5,13 @@
 # *Link Name:actual_link.file_extension
 # This might be moved to a more appropriate location
 
-# Templata Basic
-Home:
-Content:
-Test:
 
 # The One
-Home:?category=theone&id=index
-Solutions:?category=theone&id=solutions
-Contact:?category=theone&id=contact
-Hello:?category=theone&id=hello
 
-# Greenwhich
-*Home:greenwhich/home.html
-*Products:greenwhich/products.html
-*Calender:#
-*Contact:greenwhich/contact.html
-
-
+*Home:theone/index
+*Solutions:theone/solutions
+*Contact:theone/contact
+*Hello:theone/hello
 
 
             CASCADING STYLE SHEET FILES
