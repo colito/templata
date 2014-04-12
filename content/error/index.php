e71cf0bdf0e77082e467ef32ec60e8a72dd442cc
Pride Mokhele <pride.mokhele@gmail.com>
2014-04-12 19:12:59 +0200

    * need to add page name function

--- content/error/index.php
+++ content/error/index.php
@@ -1,15 +1,29 @@
 [page:Page Error]
 
 <?php
+require_once('content/constructor_kit.php');
+
+$page_handler = new PageHandler();
 
 if(!empty($_GET['category']))
 {
-    $error_type = (!empty($_GET['id']) ? $_GET['id'] : 0);
+    if(!empty($_GET['id']))
+    {
+        $_GET['id'] = (is_numeric($_GET['id']) ? $_GET['id'] : 0);
+    }
+    else
+    {
+        $_GET['id'] = 0;
+    }
+
+    $error_type = $_GET['id'];
+    $x = $page_handler->set_page_name('Error ');
 
     switch($error_type)
     {
         case 0:
-            $error_message = 'No message';
+            $error_message = '';
+            $page_handler->set_page_name('Error ' . $error_type);
             break;
         case 404:
             $error_message = '<h3>404: Oops! The page you were looking for doesn\'t seem to exist.</h3>';
