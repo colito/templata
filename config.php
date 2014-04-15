140fefa01be731d76ef547b3385348d61eede0d2
Pride Mokhele <pride.mokhele@gmail.com>
2014-04-15 15:35:02 +0200

    * added initial display content variables

--- config.php
+++ config.php
@@ -1,21 +1,28 @@
 <?php
 Class Config
 {
+    # Application name
     public $app_name = '';
-    public $landing_directory = 'theone';
-    public $landing_page = 'index';
+
+    # Initial content to be displayed
+    public $landing_category = 'theone';
+    public $landing_article = 'index';
+    public $landing_sub_article = '';
+
     public $user_name = '';
     public $password = '';
+
     public $right_click = 1; # disables right-click event on the site if set to 0. default = 1
+
+    # Template
     public $navigation_links = 'includes/nav_links.php'; # this is relative within the actual template's root directory
     public $active_template = 'templata_basic';
-    //public $active_template = 'theone';
+
     public $email_to = 'somebody@somewhere.com';
 
+    # Resources
     public $templata_content_directory = 'content';
     public $templata_images_directory = 'images';
-
-    # resources
     public $templata_libraries = 'libs';
     public $templata_jquey_path = 'libs/js/jquery';
 
