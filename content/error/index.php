62ddc7454fc8ef5182a520f7e8e80e878940d46e
Pride Mokhele <pride.mokhele@gmail.com>
2014-04-16 12:06:46 +0200

    * changed default error message '0' and made error messages bigger using <h2>

--- content/error/index.php
+++ content/error/index.php
@@ -5,36 +5,33 @@ require_once('content/constructor_kit.php');
 
 $page_handler = new PageHandler();
 
-if(!empty($_GET['category']))
+if(!empty($_GET['article']))
 {
-    if(!empty($_GET['id']))
-    {
-        $_GET['id'] = (is_numeric($_GET['id']) ? $_GET['id'] : 0);
-    }
-    else
-    {
-        $_GET['id'] = 0;
-    }
-
-    $error_type = $_GET['id'];
-    $x = $page_handler->set_page_name('Error ');
-
-    switch($error_type)
-    {
-        case 0:
-            $error_message = '';
-            $page_handler->set_page_name('Error ' . $error_type);
-            break;
-        case 404:
-            $error_message = '<h3>404: Oops! The page you were looking for doesn\'t seem to exist.</h3>';
-            break;
-        case 500:
-            $error_message = '<h3>500: Server error.</h3>';
-            break;
-        default:
-            $error_message = 'No error message for the current ID';
-            break;
-    }
-
-    echo $error_message;
+    $_GET['article'] = (is_numeric($_GET['article']) ? $_GET['article'] : 0);
 }
+else
+{
+    $_GET['article'] = 0;
+}
+
+$error_type = $_GET['article'];
+$x = $page_handler->set_page_name('Error ');
+
+switch($error_type)
+{
+    case 0:
+        $error_message = '<h2 class="error">Content not found</h2>';
+        $page_handler->set_page_name('Error ' . $error_type);
+        break;
+    case 404:
+        $error_message = '<h2 class="error">404: Oops! The page you were looking for doesn\'t seem to exist.</h2>';
+        break;
+    case 500:
+        $error_message = '<h2 class="error">500: Server error.</h2>';
+        break;
+    default:
+        $error_message = 'No error message for the current ID';
+        break;
+}
+
+echo $error_message;
