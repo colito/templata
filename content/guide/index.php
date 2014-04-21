ca2113e78c22509e3387083bcfa682357131ba56
Pride Mokhele <pride.mokhele@gmail.com>
2014-04-21 17:47:04 +0200

    * added link to placeholders documentation

--- content/guide/index.php
+++ content/guide/index.php
@@ -1,1 +1,7 @@
-<h2>Welcome to Templata</h2>
+[page:Template Guide - Home]
+
+<div id="content">
+    <h2>Welcome to Templata</h2>
+    <br>
+    <a href="guide/placeholders">Placeholders</a>
+</div>
