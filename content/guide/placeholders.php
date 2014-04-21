488eff9bf46d7960de3c72e0322d02d829636e59
Pride Mokhele <pride.mokhele@gmail.com>
2014-04-21 17:46:07 +0200

    * beggining documentation on placeholders

---
+++ content/guide/placeholders.php
@@ -0,0 +1,20 @@
+[page:Template Guide - Placeholders]
+
+<div id="content">
+    <p>All placeholders are contained within {}</p>
+
+    <p>
+        app_name <br>
+        templata_css <br>
+        base_url <br>
+        relative <br>
+        favicon <br>
+        templata_libs <br>
+        template_res <br>
+        templata_images <br>
+        templata_jquery <br>
+        validation:contact-form <br>
+        navigation_menu <br>
+        mobile_navigation_menu <br>
+    </p>
+</div>
