d34d2a1cf81a4f5179b3cdb5b9659ddd733c1637
Pride Mokhele <pride.mokhele@gmail.com>
2014-04-22 10:47:28 +0200

    * proposed new standardized placeholders

--- content/guide/placeholders.php
+++ content/guide/placeholders.php
@@ -1,20 +1,68 @@
 [page:Template Guide - Placeholders]
 
-<div id="content">
-    <p>All placeholders are contained within {}</p>
+<div id="content" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+    <p><h2>Note:</h2></p>
+    <p>All placeholders should be contained within braces ({})</p>
+
+    <br>
+
+    <p><h2>Current Placeholders</h2></p>
+    <p>
+        <ul>
+            <li>app_name</li>
+            <li>templata_css </li>
+            <li>base_url </li>
+            <li>relative </li>
+            <li>favicon </li>
+            <li>templata_libs</li>
+            <li>template_res</li>
+            <li>templata_images </li>
+            <li>templata_jquery </li>
+            <li>validation:contact-form </li>
+            <li>navigation_menu </li>
+            <li>mobile_navigation_menu </li>
+            <li>right_click</li>
+        </ul>
+    </p>
+
+    <br>
 
     <p>
-        app_name <br>
-        templata_css <br>
-        base_url <br>
-        relative <br>
-        favicon <br>
-        templata_libs <br>
-        template_res <br>
-        templata_images <br>
-        templata_jquery <br>
-        validation:contact-form <br>
-        navigation_menu <br>
-        mobile_navigation_menu <br>
+        <h2>Proposed New Placeholders</h2>
+        <h3>Navigation</h3>
+    </p>
+
+    <p>
+        File containing navigation links is stored within the includes directory
+        of the template directory.
+        <br>
+        <ul>
+            <li>navi:desktop </li>
+            <li>navi:mobile </li>
+        </ul>
+    </p>
+
+    <p><h3>Templata</h3></p>
+    <p>
+        <ul>
+            <li>templata:app-name </li>
+            <li>templata:right-click </li>
+            <li>templata:css </li>
+            <li>templata:css:filename </li>
+            <li>templata:relative </li>
+            <li>templata:libs </li>
+            <li>template:images </li>
+            <li>templata:jquery </li>
+        </ul>
+    </p>
+
+    <p><h3>Template (Template specific resources)</h3></p>
+    <p>
+        <ul>
+            <li>template:resource-name </li>
+            <li>template:css </li>
+            <li>template:css:filename </li>
+            <li>template:favicon </li>
+        <ul>
     </p>
 </div>
