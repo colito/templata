c0eed57e2de5797fed307a41b8f6ea2edb1f6edf
Pride Mokhele <pride.mokhele@gmail.com>
2014-04-22 10:45:31 +0200

    * added link to page-naming.php

--- content/guide/index.php
+++ content/guide/index.php
@@ -3,5 +3,6 @@
 <div id="content">
     <h2>Welcome to Templata</h2>
     <br>
-    <a href="guide/placeholders">Placeholders</a>
+    <a href="guide/placeholders">Placeholders</a> <br>
+    <a href="guide/page-naming">Page Naming</a> <br>
 </div>
