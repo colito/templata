90e9ecbeae5729c9cd6499d8d527c849dbbebedc
Pride Mokhele <pride.mokhele@gmail.com>
2014-04-22 10:44:48 +0200

    * documentation on how to name a page

---
+++ content/guide/page-naming.php
@@ -0,0 +1,12 @@
+[page:Page Naming]
+
+<p><h2>Naming a Page</h2></p>
+<br>
+<p>
+    To name a page, first open square brackets preferrably at the top of the script, and within those square brackets type:
+    <b>page:Page Name</b>.
+</p>
+
+<p>
+    Doing this will add the page name on the tab bar of whichever browser your site is being viewed on.
+</p>
