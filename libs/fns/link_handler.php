0ac4057d5ff929cdca475bfd0f33949ee85c25a7
Pride Mokhele <pride.mokhele@gmail.com>
2014-05-14 10:20:05 +0200

    * moved over link handling functions from operator.php to the link_handler.php

--- libs/fns/link_handler.php
+++ libs/fns/link_handler.php
@@ -1,5 +1,5 @@
 <?php
-class LinkHandler
+abstract class LinkHandler
 {
     /*** LINK MANAGEMENT ***********************************************************************/
     # needs to determine depth levels from number of directories not number of overall files
@@ -92,53 +92,6 @@ class LinkHandler
 
 
     /*** LINK EXTRACTION **************************************************/
-    # Gets href="" link
-    public function extract_hyper_links($content)
-    {
-        if(preg_match_all('/<a href=\"(.*?)\">/', $content, $link_matches) ||
-            preg_match_all('/<a href=\'(.*?)\'>/', $content, $link_matches))
-        {
-            $extracted_links = $link_matches[1];
-        }
-        else
-        {
-            $extracted_links = 'no link found';
-        }
-
-        return $extracted_links;
-    }
-
-    # Gets href="" link
-    public function extract_href_links($content)
-    {
-        if(preg_match_all('/href=\"(.*?)\"/', $content, $link_matches) ||
-            preg_match_all('/href=\'(.*?)\'/', $content, $link_matches))
-        {
-            $extracted_links = $link_matches[1];
-        }
-        else
-        {
-            $extracted_links = 'no links found';
-        }
-
-        return $extracted_links;
-    }
-
-    # Gets src="" link
-    public function extract_src_links($content)
-    {
-        if(preg_match_all('/src=\"(.*?)\"/', $content, $source_matches) ||
-            preg_match_all('/src=\'(.*?)\'/', $content, $source_matches))
-        {
-            $extracted_links = $source_matches[1];
-        }
-        else
-        {
-            $extracted_links = 'no links found';
-        }
-
-        return $extracted_links;
-    }
 
     # Gets src="" or href="" or any other link depending on the type parameeter. Default = href
     public function extract_links($content, $type = 'href')
@@ -150,60 +103,35 @@ class LinkHandler
         }
         else
         {
-            $extracted_links = 'no links found';
+            $extracted_links = false;
         }
 
         return $extracted_links;
     }
-    /*** END OF LINK EXTRACTION ******/
 
-    /*** PHYSICAL LINK ALTERING ****************************************************************/
-    # Turns standard relative hyperlinks into links that are userfriendly to the overall system
-    # Links should strictly be one level deep in order for them to work
-    # First part of the link represents a physical directory within the systems content directory
-    # and the second part represents the actual file whose content is required to be displayed.
-    public function href_link_transformer($href_source)
+    # Allows hash tag links to work normally in context of the overall system
+    public function hash_tag_links($content, $type = 'href')
     {
-        $raw_href_links = $this->extract_links($href_source);
-
-        $raw_href_links3 = array();
-        foreach($raw_href_links as $raw_href_links2)
+        if(preg_match_all('/'.$type.'=\"#(.*?)\"/', $content, $link_matches) ||
+            preg_match_all('/'.$type.'=\'#(.*?)\'/', $content, $link_matches))
         {
-            # eliminating links containing 'www' or 'http' because such links need not to be re-written
-            if(strpos($raw_href_links2, 'http') === false)
+            $extracted_links = $link_matches[1];
+            $hash_links = array();
+
+            foreach($extracted_links as $extracted_link)
             {
-                if(strpos($raw_href_links2, 'www') === false)
-                {
-                    if(strpos($raw_href_links2, '.co') === false)
-                    {
-                        if(strpos($raw_href_links2, '.html') != false || strpos($raw_href_links2, '.php') != false)
-                        {
-                            $raw_href_links3[$raw_href_links2] = $raw_href_links2;
-                        }
-                    }
-                }
+                //$hash_links[$extracted_link] = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'#'.$extracted_link;
+                $hash_links[$extracted_link] = get_current_uri(1).'#'.$extracted_link;
             }
         }
-
-        # modify urls to make them useful to the system
-        $new_href_links = array();
-        foreach($raw_href_links3 as $key => $raw_href_links4)
+        else
         {
-            # notation -> ?category=category&id=page_name
-            $exploded_link = explode('/',$raw_href_links4);
-
-            if(!empty($exploded_link[0]) && !empty($exploded_link[1]))
-            {
-                if(empty($new_href_links[$key]))
-                {
-                    $new_href_links[$key] = '?category='.$exploded_link[0].'&id='.$exploded_link[1];
-                }
-            }
+            $hash_links = false;
         }
 
-        return $new_href_links;
+        return $hash_links;
     }
-    /*** END OF PHYSICAL LINK ALTERING ***/
+    /*** END OF LINK EXTRACTION ******/
 
     /*** END OF LINK MANAGEMENT ************************/
 }
