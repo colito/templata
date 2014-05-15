511bb4124b6fa8b07b08e2c94e141a08f8aa9a5e
Pride Mokhele <pride.mokhele@gmail.com>
2014-05-15 14:34:41 +0200

    * minor clean up, only left definitions that are in use

--- includes/definitions.php
+++ includes/definitions.php
@@ -1,32 +1,13 @@
 <?php
-# DEFINITIONS ***********************************************************************/
+require_once('config.php');
+$def_config = new Config();
 
+# Declaring variables
+$content = $def_config->templata_content_directory.'/';
+$libraries = $def_config->templata_libraries.'/fns/';
 
+# DEFINITIONS :
 
-# navigation links *******************************************/
-# now replaced by navigation links above
-define('navi_home', 'home');
-//define('navi_admin', main_admin);
-
-
-# included file paths ***************************************/
-
-define('main_content', APP_ROOT_DIR.'/content');
-
-define('main_include', APP_ROOT_DIR.'/includes/');
-define('main_header', APP_ROOT_DIR.'/includes/header.php');
-define('main_footer', APP_ROOT_DIR.'/includes/footer.php');
-
-define('T_FNS', APP_ROOT_DIR.'/libs/fns/');
-
-define('main_images', '/images/');
-define('main_css', '/libs/css/');
-define('main_css_reset', '/libs/css/reset.css');
-define('main_css_normalize', '/libs/css/normalize.css');
-define('main_css_grid', '/libs/css/grid.css');
-define('main_css_style', '/libs/css/style.css');
-define('main_js', '/libs/js/');
-define('main_jquery', '/libs/js/jquery-2.0.3.min.js');
-
-
-?>
+define('T_CONTENT', $content);
+define('T_FNS', $def_config->templata_libraries.'/fns/');
+define('T_IMAGES', $def_config->templata_images_directory.'/');
