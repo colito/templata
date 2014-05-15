8a8d97b8c60f7693051571534db5de9f80779f59
Pride Mokhele <pride.mokhele@gmail.com>
2014-05-15 16:28:00 +0200

    * minor clean-up and further testing

--- content/sample/index.php
+++ content/sample/index.php
@@ -1,20 +1,5 @@
 <!--
-CSS:
-[css:reset.css]
-[css:normalize.css]
-[css:grid.css]
-[css:style.css]
-
 [page:Sample Image Gallery]
-
-<a href="jump/what.php">What's Good?!</a>
-<a href="jump/one.php">What's Good?!</a>
-<a href="jump/two.php">What's Good?!</a>
-
-<script type="text/javascript" src="js/javascript.js">
-<script type="text/javascript" src="js/jquery.js">
-<script type="text/javascript" src="js/javascript.js">
-<script type="text/javascript" src="js/javascript.js">
 -->
 
 <?php
@@ -29,5 +14,7 @@ $imagine = new Imagine();
 </div>
 
 <div id="content">
-    <?php $imagine->display_gallery('theone', 300); ?>
+    <center>
+        <?php $imagine->display_gallery('theone', 310); ?>
+    </center>
 </div>
