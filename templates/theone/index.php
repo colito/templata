583b26c663d16254bb423e4ebed9c7a6672244bd
Pride Mokhele <pride.mokhele@gmail.com>
2014-05-19 10:34:11 +0200

    * changed {page_title} to {page-title}

--- templates/theone/index.php
+++ templates/theone/index.php
@@ -5,7 +5,7 @@
     <!-- Base path: Very important especially when the systems uses clean URLs -->
     {base-url}
 
-    <title>{page_title}</title>
+    <title>{page-title}</title>
 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
