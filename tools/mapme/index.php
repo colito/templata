0139645cc22aab0eae00b43c113249a23b90a00b
Pride Mokhele <pride.mokhele@gmail.com>
2014-05-20 12:33:06 +0200

    * now uses RecursiveDirectoryIterator to map out the entire system

--- tools/mapme/index.php
+++ tools/mapme/index.php
@@ -1,99 +1,57 @@
 <?php
-require_once('../../config.php');
-$page_name = 'File Explore';
-?>
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-<?php
+# Ref: http://stackoverflow.com/questions/12077177/how-does-recursiveiteratoriterator-work-in-php
 
-//$root = $_SERVER['DOCUMENT_ROOT'].'/emp/wordworld/';
-$root = APP_ROOT_DIR;
+$path = '../../';
 
-//$current_path = '/';
+# Example 1
+//$dir  = new DirectoryIterator($path);
 
-$dir = list_directories($file);
+//var_dump($dir);
 
-function list_directories($file)
-{
-    # get a directory listing
-    $dir = array_diff (scandir (realpath($file)),
-        #folders / files to ignore
-        array ('.', '..', '.DS_Store', 'Thumbs.db', 'admin', '.idea')
-    );
+/*echo "[$path] <br>";
+foreach ($dir as $file) {
+    echo " ├ $file <br>";
+}*/
 
-    #sort folders first, then by type, then alphabetically
-    usort ($dir, create_function ('$a,$b', '
-                            return	is_dir ($a)
-                                ? (is_dir ($b) ? strnatcasecmp ($a, $b) : -1)
-                                : (is_dir ($b) ? 1 : (
-                                    strcasecmp (pathinfo ($a, PATHINFO_EXTENSION), pathinfo ($b, PATHINFO_EXTENSION)) == 0
-                                    ? strnatcasecmp ($a, $b)
-                                    : strcasecmp (pathinfo ($a, PATHINFO_EXTENSION), pathinfo ($b, PATHINFO_EXTENSION))
-                                ))
-                            ;
-                        '));
-    return $dir;
+# Example 2
+/*$files = new IteratorIterator($dir);
+
+echo "[$path] <br>";
+foreach ($files as $file) {
+    echo " ├ $file <br>";
+}*/
+
+# Example 3
+/*$dir  = new RecursiveDirectoryIterator($path);
+
+echo "[$path] <br>";
+foreach ($dir as $file) {
+    echo " |- $file <br>";
+}*/
+
+# Example 4 - The Working Example
+/*$dir  = new RecursiveDirectoryIterator($path);
+$files = new RecursiveIteratorIterator($dir);
+
+echo "[$path] <br>";
+foreach ($files as $file) {
+    if(strpos($file,'git') == false && strpos($file,'.idea') == false)
+    {
+        echo "|- $file <br>";
+    }
+}*/
+
+# Example 5 - Advanced
+$path = '.';
+$dir  = new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS);
+$files = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::SELF_FIRST);
+
+echo "[$path] <br>";
+foreach ($files as $file) {
+    $indent = str_repeat('   ', $files->getDepth());
+    if(strpos($file,'git') == false && strpos($file,'.idea') == false)
+        echo $indent, "|- $file <br>";
 }
-
-//var_dump(list_directories($file));
-//var_dump(APP_ROOT_DIR);
-//var_dump($file);
-
-?>
-
-    <div id="file_explorer" class="grid_8">
-        <a href="mapme.php"><h3>File Explorer</h3></a>
-    </div>
-
-    <div id="file_explore" class="grid_8">
-
-        <?php
-        //echo '<p class="upper">Current path:  <i class="lower">'.$current_path.'</i></p>';
-
-        if(isset($_GET['explore']))
-        {
-            echo '<a href="'.$_SERVER['HTTP_REFERER'].'">< back</a> <br>';
-
-            foreach($dir as $list_item)
-            {
-                $full_path = realpath($file.'/'.$list_item);
-
-                var_dump($full_path);
-
-                if(is_dir($full_path))
-                {
-                    echo '<a href="mapme.php?explore='.$full_path.'" class="upper">'.$list_item.'</a>';
-                    echo '<br>';
-                }
-                else if(is_file($full_path))
-                {
-                    $full_path = str_replace($root, '', $full_path);
-
-                    echo '<a href="'.navi_edit_file.'?file_path='.$full_path.'" class="lower">'.$list_item.'</a>';
-                    echo '<br>';
-                }
-            }
-        }
-        else
-        {
-            foreach($dir as $list_item)
-            {
-                $full_path = realpath($list_item);
-
-                if(is_dir($full_path))
-                {
-                    echo '<a href="mapme.php?explore='.$full_path.'" class="upper">'.$list_item.'</a>';
-                    echo '<br>';
-                }
-                else if(is_file($full_path))
-                {
-                    $full_path = str_replace($root, '', $full_path);
-
-                    echo '<a href="'.navi_edit_file.'?file_path='.$full_path.'" class="lower">'.$list_item.'</a>';
-                    echo '<br>';
-                }
-            }
-        }
-
-        ?>
-
-    </div>
