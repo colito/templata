e478fcbdb50b2f189144212d1d7d4fd92f3786e9
Pride Mokhele <pride.mokhele@gmail.com>
2014-05-21 16:18:25 +0200

    * cleaned up script contents

--- tools/mapme/index.php
+++ tools/mapme/index.php
@@ -1,85 +1,50 @@
 <?php
-
 # Ref: http://stackoverflow.com/questions/12077177/how-does-recursiveiteratoriterator-work-in-php
 
-$path = '../../';
-
-# Example 1
-//$dir  = new DirectoryIterator($path);
-
-//var_dump($dir);
-
-/*echo "[$path] <br>";
-foreach ($dir as $file) {
-    echo " ├ $file <br>";
-}*/
-
-# Example 2
-/*$files = new IteratorIterator($dir);
-
-echo "[$path] <br>";
-foreach ($files as $file) {
-    echo " ├ $file <br>";
-}*/
-
-# Example 3
-/*$dir  = new RecursiveDirectoryIterator($path);
-
-echo "[$path] <br>";
-foreach ($dir as $file) {
-    echo " |- $file <br>";
-}*/
-
-# Example 4 - The Working Example
-/*$dir  = new RecursiveDirectoryIterator($path);
-$files = new RecursiveIteratorIterator($dir);
-
-echo "[$path] <br>";
-foreach ($files as $file) {
-    if(strpos($file,'git') == false && strpos($file,'.idea') == false)
-    {
-        echo "|- $file <br>";
-    }
-}*/
-
-# Example 5 - Advanced
 $path = '.';
 $dir  = new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS);
 $files = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::SELF_FIRST);
 
-define('DEFAULT_COLOR', '<b style=color:slategrey;>|-- </b>');
-define('ROOT_PIPE', '<b style=color:red;>|-- </b>');
-define('LEVEL_2', '<b style=color:green;>|-- </b>');
-define('LEVEL_3', '<b style=color:dodgerblue;>|-- </b>');
-define('LEVEL_4', '<b style=color:goldenrod;>|-- </b>');
+function determine_pipe_color($depth)
+{
+    $default_color = '<b style=color:slategrey;>|-- </b>';
+    $root_pipe = '<b style=color:red;>|-- </b>';
+    $level_2 = '<b style=color:green;>|-- </b>';
+    $level_3 = '<b style=color:dodgerblue;>|-- </b>';
+    $level_4 = '<b style=color:goldenrod;>|-- </b>';
+
+    switch($depth)
+    {
+        case 1:
+            $pipe_color = $root_pipe;
+            break;
+        case 2:
+            $pipe_color = $level_2;
+            break;
+        case 3:
+            $pipe_color = $level_3;
+            break;
+        case 4:
+            $pipe_color = $level_4;
+            break;
+        default:
+            $pipe_color = $default_color;
+    }
+
+    return $pipe_color;
+}
 
 echo '['.$path.'] <br>';
 foreach ($files as $key => $file) {
     $indent = str_repeat('&nbsp;&nbsp;&nbsp;', $files->getDepth());
     if(strpos($file,'git') == false && strpos($file,'.idea') == false)
     {
-        $depth = $files->getDepth();
+        //$depth = $files->getDepth();
         $split = explode('/', $file);
 
         $new_depth = count($split) - 1;
 
-        switch($new_depth)
-        {
-            case 1:
-                $pipe_color = ROOT_PIPE;
-                break;
-            case 2:
-                $pipe_color = LEVEL_2;
-                break;
-            case 3:
-                $pipe_color = LEVEL_3;
-                break;
-            case 4:
-                $pipe_color = LEVEL_4;
-                break;
-            default:
-                $pipe_color = DEFAULT_COLOR;
-        }
+        $pipe_color = determine_pipe_color($new_depth);
 
         if(is_dir($file))
         {
