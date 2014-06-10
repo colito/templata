3139d83b594d7b86d5a67479ceb4e35418b41d12
Pride Mokhele <pride.mokhele@gmail.com>
2014-06-10 10:55:43 +0200

    * testing picasa slideshow

---
+++ content/sample/slide-show.php
@@ -0,0 +1,9 @@
+[page:Picasa Slideshow]
+
+<div id="content" class="grid_12">
+    <embed type="application/x-shockwave-flash" src="https://photos.gstatic.com/media/slideshow.swf" width="500" height="500"
+           flashvars="host=picasaweb.google.com&captions=1&hl=en_US&feat=flashalbum&RGB=0x000000&feed=https%3A%2F%2Fpicasaweb.google.com%2Fdata%2Ffeed%2Fapi%2Fuser%2F103181077861626467186%2Falbumid%2F5987591169503517041%3Falt%3Drss%26kind%3Dphoto%26authkey%3DGv1sRgCOrjwNSGwJ6KZQ%26hl%3Den_US"
+           pluginspage="http://www.macromedia.com/go/getflashplayer">
+
+    </embed>
+</div>
