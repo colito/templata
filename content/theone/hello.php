9c849fbb2a077902b11c1e1be8d2d8106561eb4e
Pride Mokhele <pride.mokhele@gmail.com>
2014-06-12 15:45:18 +0200

    * removed test header links

--- content/theone/hello.php
+++ content/theone/hello.php
@@ -1,11 +1,5 @@
 [page:Hello World]
 
-[head:template-css:style.css]
-[head:template-js:javascript.js]
-
-[head:templata-css:style.css]
-[head:templata-js:link/to/other/file]
-
 <?php
     $hello = 'Hello World!! From PHP.';
     echo '<h2>'.$hello.'</h2>';
