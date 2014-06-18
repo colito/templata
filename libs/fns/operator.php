246e97d01959c81304ffc808838d51a933da7022
Pride Mokhele <pride.mokhele@gmail.com>
2014-06-18 11:55:56 +0200

    * moved get_browser() from operator.php to functions.php

--- libs/fns/operator.php
+++ libs/fns/operator.php
@@ -28,10 +28,7 @@ abstract class Operator extends LinkHandler
     {
         $x = $this->link_handler();
 
-        if($x == false)
-        {
-            return false;
-        }
+        if($x == false) {return false;}
 
         $output = '';
         foreach($x['nav_links'] as $y)
@@ -55,10 +52,7 @@ abstract class Operator extends LinkHandler
         $active_template = $config->active_template;
         $links_file = 'templates/'.$active_template.'/'.$links;
 
-        if(!file_exists($links_file))
-        {
-            return false;
-        }
+        if(!file_exists($links_file)) {return false;}
 
         $lines = file($links_file, FILE_IGNORE_NEW_LINES);
 
@@ -69,10 +63,7 @@ abstract class Operator extends LinkHandler
             # checks array for an occupied array index.
             if(!empty($line))
             {
-                if(preg_match_all("/\[(css:.*?)\]/", $line, $css_matches))
-                {
-                    $clean_lines[] = $css_matches[1];
-                }
+                if(preg_match_all("/\[(css:.*?)\]/", $line, $css_matches)) {$clean_lines[] = $css_matches[1];}
             }
         }
 
@@ -158,10 +149,7 @@ abstract class Operator extends LinkHandler
         $html_head_links = '';
         if(is_array($header_resources))
         {
-            if(empty($header_resources))
-            {
-                return '';
-            }
+            if(empty($header_resources)) {return '';}
 
             foreach($header_resources as $header_resource)
             {
@@ -207,92 +195,4 @@ abstract class Operator extends LinkHandler
         return $lib_array;
     }
     /*** END OF SOURCE FILES*****************/
-
-    /*** BROWSER ***********************************************************/
-    # Ref: http://www.php.net/get_browser
-    public function get_browser()
-    {
-        $u_agent = $_SERVER['HTTP_USER_AGENT'];
-        $bname = 'Unknown';
-        $platform = 'Unknown';
-        $version= "";
-
-        //First get the platform?
-        if (preg_match('/linux/i', $u_agent)) {
-            $platform = 'linux';
-        }
-        elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
-            $platform = 'mac';
-        }
-        elseif (preg_match('/windows|win32/i', $u_agent)) {
-            $platform = 'windows';
-        }
-
-        // Next get the name of the useragent yes seperately and for good reason
-        if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent))
-        {
-            $bname = 'Internet Explorer';
-            $ub = "MSIE";
-        }
-        elseif(preg_match('/Firefox/i',$u_agent))
-        {
-            $bname = 'Mozilla Firefox';
-            $ub = "Firefox";
-        }
-        elseif(preg_match('/Chrome/i',$u_agent))
-        {
-            $bname = 'Google Chrome';
-            $ub = "Chrome";
-        }
-        elseif(preg_match('/Safari/i',$u_agent))
-        {
-            $bname = 'Apple Safari';
-            $ub = "Safari";
-        }
-        elseif(preg_match('/Opera/i',$u_agent))
-        {
-            $bname = 'Opera';
-            $ub = "Opera";
-        }
-        elseif(preg_match('/Netscape/i',$u_agent))
-        {
-            $bname = 'Netscape';
-            $ub = "Netscape";
-        }
-
-        // finally get the correct version number
-        $known = array('Version', $ub, 'other');
-        $pattern = '#(?<browser>' . join('|', $known) .
-            ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
-        if (!preg_match_all($pattern, $u_agent, $matches)) {
-            // we have no matching number just continue
-        }
-
-        // see how many we have
-        $i = count($matches['browser']);
-        if ($i != 1) {
-            //we will have two since we are not using 'other' argument yet
-            //see if version is before or after the name
-            if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
-                $version= $matches['version'][0];
-            }
-            else {
-                $version= $matches['version'][1];
-            }
-        }
-        else {
-            $version= $matches['version'][0];
-        }
-
-        // check if we have a number
-        if ($version==null || $version=="") {$version="?";}
-
-        return array(
-            'userAgent' => $u_agent, #user agent
-            'name'      => $bname, #browser name
-            'version'   => $version, # browser version
-            'platform'  => $platform, # OS
-            'pattern'    => $pattern # regex pattern
-        );
-    }
 }
