ffb39b7c5ce5ca00a6d7f4e041b758346cf78e63
Pride Mokhele <pride.mokhele@gmail.com>
2014-07-21 21:27:36 +0200

    * testing sound cloud iframe

---
+++ content/sample/sc.php
@@ -0,0 +1,1 @@
+<iframe allowtransparency="true" scrolling="no" frameborder="no" src="https://w.soundcloud.com/icon/?url=http%3A%2F%2Fsoundcloud.com%2Fmr-pride&color=orange_white&size=32" style="width: 32px; height: 32px;"></iframe>
