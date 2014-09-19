20fd8cb54a2fa30b543a19997905ca0dae1215b5
Pride <pride.mokhele@gmail.com>
2014-09-19 17:07:52 +0200

    * testing out the new instantiate_class function

--- content/sample/index.php
+++ content/sample/index.php
@@ -4,9 +4,15 @@
 
 <?php
 
-include_lib('imagine');
+$imagine = instantiate_class('Imagine');
+$check = instantiate_class('PageHandler', 'page_handler');
 
-$imagine = new Imagine();
+$functons = get_defined_functions();
+
+var_dump(get_class_methods('PageHandler'));
+var_dump($functons['user']);
+
+
 ?>
 
 <div id="content" class="grid_12">
