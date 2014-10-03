20c8599ec24c3c1e8022a2cb6c74eba138dba0f5
Pride <pride.mokhele@gmail.com>
2014-10-03 14:26:25 +0200

    * fixed some bugs

--- libs/fns/models/dbi_negotiator.php
+++ libs/fns/models/dbi_negotiator.php
@@ -1,5 +1,5 @@
 <?php
-include_lib('db_interrogator');
+include_lib('models/db_interrogator');
 class DbINegotiator extends DbInterrogator
 {
     public $columns = array();
@@ -30,6 +30,26 @@ class DbINegotiator extends DbInterrogator
         return $columns_to_save;
     }
 
+    public function get_id($where = '1=1')
+    {
+        $table = $this->db_table;
+        $id = $this->get_data($table, 'id', $where);
+        return $id[0]['id'];
+    }
+
+    public function select_data($column, $where = '1=1')
+    {
+        $table = $this->db_table;
+        $result = $this->get_data($table, $column, $where);
+
+        return $result;
+    }
+
+    public function get_column($column_name)
+    {
+        return $this->get_data($this->db_table, $column_name);
+    }
+
     public function save($check = 0, $check_column = null, $check_value = null)
     {
         $table = $this->db_table;
@@ -38,20 +58,20 @@ class DbINegotiator extends DbInterrogator
         switch($check)
         {
             case 1:
-                $where = $check_column .' = "' . $check_value.'"';
+                //$where = $check_column .' = "' . $check_value.'"';
 
-                $exists = $this->record_exists($where);
-                if($exists) { return 'record already exists'; }
+                $exists = $this->record_exists($check_value);
+                if($exists) { return array('result'=>false, 'message'=>'record already exists'); }
                 break;
 
             case 2:
                 $this->update_data($table, $check_column, $check_value);
-                return 'record updated';
+                return array('result'=>true, 'message'=>'record updated');
                 break;
         }
 
         $this->insert_data($table, $columns);
 
-        return true;
+        return array('result'=>true, 'message'=>'record saved');
     }
 }
