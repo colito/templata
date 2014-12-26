00b4b4b3a2de4a1d95433334ac8865f503977873
Pride Mokhele <pride.mokhele@gmail.com>
2014-12-26 17:04:16 +0200

    * experimenting with making config.php dynamic

--- config/config.php
+++ config/config.php
@@ -1,34 +1,13 @@
 <?php
 Class Config
 {
-    # App name
-    public $app_name = 'Templata';
+    function __construct($class_file = 'Application')
+    {
+        $file_name = strtolower($class_file).'.php';
+        require_once('config/'.$file_name);
 
-    # Initial content to be displayed
-    public $default_landing_path = 'theone';
-
-    # DB
-    public $host = 'localhost';
-    public $user_name = 'root';
-    public $password = '';
-    public $database = '';
-
-    # Right click bahaviour
-    public $right_click = 1; # disables right-click event on the site if set to 0. default = 1
-
-    # Template
-    public $navigation_links = 'includes/links.php'; # this is relative within the actual template's root directory
-    public $active_template = 'templata_basic';
-
-    # Email
-    public $email_to = 'somebody@somewhere.com';
-
-    # Resources
-    public $templata_content_directory = 'content';
-    public $templata_images_directory = 'images';
-    public $templata_libraries = 'libs';
-    public $templata_jquey_path = 'libs/js/jquery';
-
+        var_dump($file_name);
+    }
 }
 
 # application root
