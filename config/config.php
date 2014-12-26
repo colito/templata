1e08d9e1320e02d0be9b893e4714c24492f1321d
Pride Mokhele <pride.mokhele@gmail.com>
2014-12-26 17:36:48 +0200

    * this config file is now dynamic

--- config/config.php
+++ config/config.php
@@ -1,13 +1,19 @@
 <?php
-Class Config
-{
-    function __construct($class_file = 'Application')
-    {
-        $file_name = strtolower($class_file).'.php';
-        require_once('config/'.$file_name);
 
-        var_dump($file_name);
-    }
+function TConfig($class = 'Application')
+{
+    $config = init($class);
+    return $config;
+}
+
+function init($class)
+{
+    $file_name = strtolower($class).'.php';
+    require_once('config/'.$file_name);
+
+    $class_name = $class.'Config';
+
+    return new $class_name();
 }
 
 # application root
