a65ced4441dd32f3ec6a11b23616056267c39174
Pride Mokhele <pride.mokhele@gmail.com>
2014-12-26 15:01:39 +0200

    * moved imagine.php to root of libs directory

---
+++ libs/imagine.php
@@ -0,0 +1,82 @@
+<?php
+# Image manipulation library
+class Imagine
+{
+    public $image_directory = T_IMAGES;
+
+    # Checks if the gallery (directory) suggested exists
+    public function verify_destination($destination)
+    {
+        # returns false if the directory the user specified doesn't exist
+        if(!file_exists($destination))
+            return false;
+        elseif(!is_dir($destination))
+            return false;
+        else
+            return true;
+    }
+
+    # Verifies is the file is indeed an image
+    public function valid_image($image_location, $return_info = 0)
+    {
+       $result = getimagesize($image_location);
+
+        if(!$result)
+            return false;
+
+        if($return_info == 1) # Returns getimagesize() output if the user is interested in it.
+            return $result;
+        else
+            return true;
+    }
+
+    # Retrieves a list of images
+    public function image_list($image_directory)
+    {
+        # returns false if the directory the user specified doesn't exist
+        if(!$this->verify_destination($image_directory))
+            return false;
+
+        $imagelist = array();
+
+        foreach(glob($image_directory.'*.*') as $filename)
+            $imagelist[] = $filename;
+
+        return $imagelist;
+    }
+
+    # Outputs images from a specified gallery
+    public function display_gallery($gallary, $x_dimension = 180)
+    {
+        $image_gallery = $this->image_directory.$gallary.'/';
+        $imagelist = $this->image_list($image_gallery);
+
+        # returns false if the directory the user specified doesn't exist
+       if(!$imagelist)
+           return false;
+
+       foreach($imagelist as $filename)
+            # Makes sure that the image is a valid one before it gets displayed
+           if($this->valid_image($filename))
+                echo '<img src="'.$filename.'" width="'.$x_dimension.'"> &nbsp;';
+    }
+
+    public function create_image_thumb($image)
+    {
+        try
+        {
+            $thumb = new Imagick($image);
+
+            $thumb->resizeImage(320,240,Imagick::FILTER_LANCZOS,1);
+            $thumb->writeImage($image);
+
+            $thumb->destroy();
+
+            return true;
+        }
+        catch(Exception $e)
+        {
+            return 'Could not create thumb: ' . $e;
+        }
+    }
+}
