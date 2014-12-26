9c23b3839dcd4ed9e203bc7c9e5657da0809573d
Pride Mokhele <pride.mokhele@gmail.com>
2014-12-26 15:46:45 +0200

    * changed footer copyright

--- templates/templata_basic/index.php
+++ templates/templata_basic/index.php
@@ -81,7 +81,7 @@
 
     <div id='footer' class='grid_12'>
         <div id='copyright'>
-            <p>&copy; 2013 Success Motivation. All Rights Reserved</p>
+            <p>&copy; 2013 Templata. All Rights Reserved</p>
         </div>
     </div>
 </div>
