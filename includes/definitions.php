042b5b9f52211fb3a7eb137724b466b9a8ed0f33
Pride Mokhele <pride.mokhele@gmail.com>
2014-12-26 14:56:50 +0200

    * added new definitions and modified some of the other to suit the new structure

--- includes/definitions.php
+++ includes/definitions.php
@@ -8,11 +8,15 @@ $libraries = $def_config->templata_libraries.'/fns/';
 
 # DEFINITIONS :
 
-define('T_MODEL', 'models/');
+//define('T_MODEL', 'models/');
 define('T_CONTROLLER', 'controllers/');
 
 define('T_CONTENT', $content);
-define('T_LIBS', $def_config->templata_libraries);
-define('T_FNS', $def_config->templata_libraries.'/fns/');
+define('T_LIBS', $def_config->templata_libraries.'/');
+define('T_SYSTEM', T_LIBS.'system/');
+define('T_SYS', T_LIBS.'system/');
+define('T_MODELS', T_LIBS.'models/');
+define('T_MODEL', T_LIBS.'models/');
+define('T_FNS', T_LIBS.'fns/');
 define('T_IMAGES', $def_config->templata_images_directory.'/');
 define('T_TOOLS', 'tools/');
