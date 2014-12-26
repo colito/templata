23c3a1b7e90f4c70578efc9c08d5dbbb0f33a8de
Pride Mokhele <pride.mokhele@gmail.com>
2014-12-26 15:15:20 +0200

    * moved to test directory

--- test/index2.php
+++ test/index2.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require_once('config.php');
-require_once(APP_ROOT_DIR.'/includes/definitions.php');
+require_once(APP_ROOT_DIR . '/includes/definitions.php');
 require_once('libs/fns/page_handler.php');
 
 
