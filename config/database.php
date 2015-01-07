2a1c58152df70d99103d33f951adc34b506ab402
Pride <pride.mokhele@gmail.com>
2015-01-07 12:16:06 +0200

    * is now able to automatically switch between two sets of settings

--- config/database.php
+++ config/database.php
@@ -2,10 +2,30 @@
 Class DatabaseConfig
 {
     # DB
-    public $host = 'localhost';
-    public $user_name = 'root';
-    public $password = '';
-    public $database = '';
+    public $host, $user_name, $password, $database;
 
+    function __construct()
+    {
+        if($_SERVER['HTTP_HOST'] == 'localhost') {$this->development();}
+        else {$this->production();}
+    }
+
+    # Fill in the credentials for your development environment in this function
+    private function development()
+    {
+        $this->host = 'localhost';
+        $this->user_name = 'root';
+        $this->password = '';
+        $this->database = '';
+    }
+
+    # Fill in the credentials for the production environment in this function
+    private function production()
+    {
+        $this->host = 'localhost';
+        $this->user_name = '';
+        $this->password = '';
+        $this->database = '';
+    }
 }
 
